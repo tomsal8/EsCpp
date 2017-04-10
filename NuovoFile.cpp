0f84a819f469be0448160b3784d14a592be71596
tomsal8 <tommasosalerno3@gmail.com>
2017-04-10 19:10:18 +0200

    Creato file

---
+++ NuovoFile.cpp
@@ -0,0 +1,18 @@
+
+//New code for exploring of Atom
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  /* code */
+
+
+  return 0;
+}
